3be5c7a91963b6e0e34dd176119de4e6116a7d6c
YukiSat0u <122474896+YukiSat0u@users.noreply.github.com>
2023-08-01 11:17:37 +0300

    hw29

---
+++ FineGrainedQueue.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+struct Node
+{
+	int value;
+	Node* next;
+	std::mutex* node_mutex;
+};
+
+class FineGrainedQueue
+{
+public:
+	Node* head;
+
+	FineGrainedQueue()
+	{
+		head = nullptr; // Initialize head to nullptr
+	}
+
+	void insertIntoMiddle(int value, int pos);
+
+private:
+	std::mutex queue_mutex;
+};
